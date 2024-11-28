c097f9389470429133223281c6bf8de59e2034dd
alrashid2001 <alrashidissa2001@hotmail.com>
2024-11-28 12:30:40 +0000

    Auto Create Dir For Database.

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+# Initialize Dir for Database
+import os
+import sys
+
+# Set the base directory relative to the script's location
+MAIN_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
+
+print("Main" + str(MAIN_DIR))
+os.makedirs(f"{MAIN_DIR}/database/arcive", exist_ok=True)
+os.makedirs(f"{MAIN_DIR}/database/sql", exist_ok=True)
