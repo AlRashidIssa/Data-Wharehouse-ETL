cb805ef47cdc805f58cf669b0d45f387f5964465
AlRashid Issa <147489627+AlRashidIssa@users.noreply.github.com>
2024-12-01 22:45:05 +0000

    Create Readme file for read Configration.

---
+++ src/config/__init__.py
@@ -0,0 +1,66 @@
+import yaml
+import os
+import sys
+from typing import Dict, Any, Optional
+
+from pathlib import Path
+
+# Dynamically locate the main project directory
+current_dir = Path(__file__).resolve().parent
+MAIN_DIR = current_dir
+while MAIN_DIR.name != "Data-Wharehouse-ETL":
+    MAIN_DIR = MAIN_DIR.parent
+# Get the absolute path to the directory one level above the current file's directory
+print(MAIN_DIR)
+CONFIGDIR = os.path.join(MAIN_DIR, "configs")
+print(CONFIGDIR)  # Prints the absolute path to the 'configs' directory
+
+sys.path.append(f"{MAIN_DIR}/src")
+
+# Importing logging utilities (assuming they are implemented in 'utils')
+from utils import ErrorTrack, PipelineTrack
+
+def config_yaml_reader(file_path: Optional[str] = None) -> Dict[str, Any]:
+    """
+    Reads a YAML configuration file and returns its contents as a dictionary.
+
+    Args:
+        file_path (str): Path to the YAML file.
+
+    Returns:
+        Dict[str, Any]: Parsed content of the YAML file.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        yaml.YAMLError: If there is an error while parsing the YAML file.
+    """
+    try:
+        # If not, look for any YAML file in the specified directory
+        config_files = os.listdir(CONFIGDIR)
+        for con in config_files:
+            if con.endswith(".yaml"):
+                # Set the first YAML file found as the configuration file
+                file_path = os.path.join(CONFIGDIR, con)
+                PipelineTrack(f"Using alternative configuration file: {file_path}")
+                break
+            else:
+                # If no YAML file is found, raise an error
+                error_msg = "No YAML configuration file found in the directory."
+                ErrorTrack(error_msg)
+                raise FileNotFoundError(error_msg)
+
+        # Load the YAML file
+        with open(file_path, 'r') as f:
+            config = yaml.safe_load(f)
+            PipelineTrack("Successfully loaded configuration from YAML file.")
+            return config
+    except FileNotFoundError as e:
+            ErrorTrack(f"File not found error: {e}")
+            raise
+    except yaml.YAMLError as e:
+        ErrorTrack(f"Error parsing YAML file: {e}")
+        raise ValueError(f"Error parsing YAML file: {e}")
+
+
+if __name__ == "__main__":
+    configs = config_yaml_reader(None)
